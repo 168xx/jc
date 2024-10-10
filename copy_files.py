987a3b674b328a110e5d2d62d669217c9bb03c08
baidu <web@baidu.com>
2024-10-10 15:50:50 +0800

    更新

--- copy_files.py
+++ copy_files.py
@@ -1,24 +1,25 @@
 import os  
 import shutil  
-import sys  
   
-def copy_files_from_dir(src_dir, dest_dir):  
-    for filename in os.listdir(src_dir):  
-        if filename.endswith('.txt') or filename.endswith('.m3u'):  
-            src_file = os.path.join(src_dir, filename)  
-            dest_file = os.path.join(dest_dir, filename)  
-              
-            # Copy the file to the destination directory  
-            shutil.copy2(src_file, dest_dir)  
-            print(f"Copied {src_file} to {dest_dir}")  
+def copy_files(src_dir, dest_dir):  
+    if not os.path.exists(dest_dir):  
+        os.makedirs(dest_dir)  
+      
+    for root, dirs, files in os.walk(src_dir):  
+        for file in files:  
+            if file.endswith('.txt') or file.endswith('.m3u'):  
+                src_file = os.path.join(root, file)  
+                dest_file = os.path.join(dest_dir, file)  
+                shutil.copy2(src_file, dest_file)  
   
 if __name__ == "__main__":  
+    import sys  
+      
     if len(sys.argv) != 2:  
-        print("Usage: python copy_files.py <private_repo_tv_directory>")  
+        print("Usage: python3 copy_files.py <source_directory>")  
         sys.exit(1)  
       
-    private_repo_tv_dir = sys.argv[1]  
-    current_dir = os.getcwd()  
+    src_directory = sys.argv[1]  
+    dest_directory = os.getcwd()  # 当前工作目录，即GitHub Actions的工作区  
       
-    # Copy files from the tv directory in the private repo to the current directory  
-    copy_files_from_dir(private_repo_tv_dir, current_dir)
+    copy_files(src_directory, dest_directory)
